484ec2c698de89fd295c5149f97fc0f9f082f789
botir <botirjp@gmail.com>
2025-08-12 16:08:32 +0900

    Implement code changes to enhance functionality and improve performance

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header(){
     return(
-        <nav className="font-nico sticky top-0 flex justify-between items-center bg-black p-4">
+        <nav className="font-nico sticky top-0 flex justify-evenly items-center bg-black p-4">
             <div className="text-red-600"><a href="#home">LOGO</a></div>
             <div className="text-white"><a href="#home">BOSH SAHIFA</a></div>
             <div className="text-white"><a href="#about">BIZ HAQIMIZDA</a></div>
--- frontend/src/components/SamuraiSection/SamuraiSection.jsx
+++ frontend/src/components/SamuraiSection/SamuraiSection.jsx
@@ -1,7 +1,12 @@
 export default function SamuraiSection() {
   return (
     <section id="home" className="h-screen bg-black flex items-center justify-center">
-      <h1 className="font-nico text-6xl text-white">OMOTENASHI ACADEMY</h1>
+      <img 
+        src="Sun.png" 
+        alt="Sun" 
+        className="absolute top-center left-0 z-1"
+      />
+      <h1 className="font-nico text-6xl text-white z-10">OMOTENASHI ACADEMY</h1>
     </section>
   );
 }
